6a8de502734aa705ea467b7c08defa81f215e86b
Priyanshu-2024 <priyanshu.patel@iflair.com>
2024-05-27 17:08:59 +0530

    First Commit of this project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GameScreenComponent } from './game-screen/game-screen.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:GameScreenComponent,
+  },
+  
+  {
+    path:'gameScreen',
+    component:GameScreenComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
